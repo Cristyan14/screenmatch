fbbd815fcdead7dc342239be094b9933960a12f1
Cristyan <lisbo.h2o@gmail.com>
2025-06-27 20:11:39 -0300

    sobe

--- src/main/java/br/estudos/ScreenmatchApplication.java
+++ src/main/java/br/estudos/ScreenmatchApplication.java
@@ -1,14 +1,17 @@
 package br.estudos;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.estudos.principal.Principal;
+import br.repository.SerieRepository;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
-
+	@Autowired
+	private SerieRepository repositorio;
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
 	}
@@ -17,7 +20,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
-		Principal principal = new Principal();
+		Principal principal = new Principal(repositorio);
 		principal.exibeMenu();
 		
 	
---
+++ src/main/java/br/repository/SerieRepository.java
@@ -0,0 +1,9 @@
+package br.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.model.Serie;
+
+public interface SerieRepository extends JpaRepository<Serie, Long>{
+
+}
